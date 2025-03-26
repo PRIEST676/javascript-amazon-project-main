fabd10195376686c186066e76ef7fca8d6e49a5c
PRIEST676 <Anushikbs676@gmail.com>
2025-03-26 20:46:22 +0530

    created automated testing scripts

---
+++ tests/moneyTest.js
@@ -0,0 +1,27 @@
+import { formatCurreny } from "../scripts/utils/money.js";
+
+console.log('converts cents into dollars');
+
+if (formatCurreny(2095) == '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+
+
+console.log('works with 0');
+
+if(formatCurreny(0) === '0.00'){
+  console.log('passed')
+}else{
+  console.log('failed')
+}
+
+console.log('rounds up to nearest cent');
+
+if(formatCurreny(2000.5) === '20.01'){
+  console.log('passed')
+}else{
+  console.log('failed')
+}
